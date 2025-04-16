d741c6c8c7b9755b87d1608f5a1301315f599f80
Sunnywang1001 <kent721595@gmail.com>
2025-04-16 12:16:03 +0800

    week09

--- week09-3.cpp
+++ week09-3.cpp
@@ -1,1 +1,18 @@
+//week09-3.cpp
+//yeh001_star
+
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=i; j++){
+			printf("*");
+	    }
+		printf("\n", i);
+
+	}
+}
 
